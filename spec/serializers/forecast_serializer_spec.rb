6bb934e9d855e1b6111acea96841f7af03e3f867
Xander Hendry <vhendry.coding@gmail.com>
2024-01-14 19:14:14 -0500

    add forecast serializer

--- spec/serializers/forecast_serializer_spec.rb
+++ spec/serializers/forecast_serializer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe ForecastSerializer do
     response = ForecastSerializer.new(WeatherService.forecast_at_location({lat: '33.95', lon: '-81.12'}, 5))
     output = response.serialize_json
     expect(output).to have_key(:data)
-    expect(output[:data]).to be_an(Array)
+    expect(output[:data]).to be_an(Hash)
     
-    weather_data = output[:data].first
+    weather_data = output[:data]
     expect(weather_data).to have_key(:id)
     expect(weather_data[:id]).to be_nil
     expect(weather_data).to have_key(:type)
@@ -22,7 +22,7 @@ RSpec.describe ForecastSerializer do
     expect(weather_attributes[:daily_weather].count).to eq(5)
     expect(weather_attributes).to have_key(:hourly_weather)
     expect(weather_attributes[:hourly_weather]).to be_a(Array)
-    expect(weather_attributes[:hourly_weather].count).to eq(24)
+    expect(weather_attributes[:hourly_weather].count).to eq(120)
     current_weather = weather_attributes[:current_weather]
     expect(current_weather).to have_key(:last_updated)
     expect(current_weather[:last_updated]).to be_a(String)
@@ -31,7 +31,7 @@ RSpec.describe ForecastSerializer do
     expect(current_weather).to have_key(:feels_like)
     expect(current_weather[:feels_like]).to be_a(Float)
     expect(current_weather).to have_key(:humidity)
-    expect(current_weather[:humidity]).to be_a(Float)
+    expect(current_weather[:humidity]).to be_a(Integer)
     expect(current_weather).to have_key(:uvi)
     expect(current_weather[:uvi]).to be_a(Float)
     expect(current_weather).to have_key(:visibility)
@@ -39,7 +39,7 @@ RSpec.describe ForecastSerializer do
     expect(current_weather).to have_key(:condition)
     expect(current_weather[:condition]).to be_a(String)
     expect(current_weather).to have_key(:icon)
-    # expect(current_weather[:icon]).to be_a(PNG)
+    expect(current_weather[:icon]).to be_a(String)
     daily_weather = weather_attributes[:daily_weather].first
     expect(daily_weather).to have_key(:date)
     expect(daily_weather[:date]).to be_a(String)
@@ -54,16 +54,16 @@ RSpec.describe ForecastSerializer do
     expect(daily_weather).to have_key(:condition)
     expect(daily_weather[:condition]).to be_a(String)
     expect(daily_weather).to have_key(:icon)
-    # expect(daily_weather[:icon]).to be_a(PNG)
+    expect(daily_weather[:icon]).to be_a(String)
     hourly_weather = weather_attributes[:hourly_weather].first
-    expect(daily_weather).to have_key(:time)
-    expect(daily_weather[:time]).to be_a(String)
-    expect(daily_weather).to have_key(:temperature)
-    expect(daily_weather[:temperature]).to be_a(Float)
-    expect(daily_weather).to have_key(:conditions)
-    expect(daily_weather[:conditions]).to be_a(String)
-    expect(daily_weather).to have_key(:icon)
-    # expect(daily_weather[:icon]).to be_a(PNG)
+    expect(hourly_weather).to have_key(:time)
+    expect(hourly_weather[:time]).to be_a(String)
+    expect(hourly_weather).to have_key(:temperature)
+    expect(hourly_weather[:temperature]).to be_a(Float)
+    expect(hourly_weather).to have_key(:conditions)
+    expect(hourly_weather[:conditions]).to be_a(String)
+    expect(hourly_weather).to have_key(:icon)
+    expect(hourly_weather[:icon]).to be_a(String)
 
   end
 end
